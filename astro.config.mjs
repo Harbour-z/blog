f9a11249855b7eac52db2ff8afd7e05d74632532
Zhidong Zhang <814287468@qq.com>
2025-01-13 03:30:47 +0800

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://harbour-z.github.io',
 	integrations: [mdx(), sitemap()],
+	base: 'blog'
 });
